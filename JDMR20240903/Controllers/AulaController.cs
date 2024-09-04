391e0d72e50b0927bc0ac5661895295ab7b0c89f
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-03 18:26:01 -0600

    Agregar archivos de proyecto.

---
+++ JDMR20240903/Controllers/AulaController.cs
@@ -0,0 +1,69 @@
+﻿using JDMR20240903.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JDMR20240903.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AulaController : ControllerBase
+    {
+        static List<Aula> aulas = new List<Aula>();
+
+        // obtener todas las aulas
+        [HttpGet]
+        public IEnumerable<Aula> Get()
+        {
+            return aulas;
+        }
+
+        //obtener un aula por id
+        [HttpGet("{id}")]
+        public Aula? Get(int id)
+        {
+            var aula = aulas.FirstOrDefault(x => x.Id == id);
+            return aula;
+        }
+
+        //crear un aula
+        [HttpPost]
+        public IActionResult Post([FromBody] Aula aula)
+        {
+            aulas.Add(aula);
+            return Ok();
+        }
+
+        //actualizar un aula por id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Aula aula)
+        {
+            var aulaToUpdate = aulas.FirstOrDefault(x => x.Id == id);
+
+            if (aulaToUpdate == null)
+            {
+                return NotFound();
+            }
+         
+            aulaToUpdate.Nombre = aula.Nombre;
+            aulaToUpdate.Descripcion = aula.Descripcion;
+
+            return Ok();
+        }
+
+        //eliiinar un aula por id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var aulaToDelete = aulas.FirstOrDefault(x => x.Id == id);
+
+            if (aulaToDelete == null)
+            {
+                return NotFound();
+            }
+
+            aulas.Remove(aulaToDelete);
+            return Ok();
+        }
+    }
+}
---
+++ JDMR20240903/Models/Aula.cs
@@ -0,0 +1,9 @@
+﻿namespace JDMR20240903.Models
+{
+    public class Aula
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
